aa53576743e5ff1b9a4268fdf80a05785b35157b
DevJsmJsr <jsmajsr@outlook.com>
2024-11-24 16:19:01 -0500

    send request test1

--- VAdmin/core/views/read_property_card.py
+++ VAdmin/core/views/read_property_card.py
@@ -12,12 +12,13 @@ import base64
 
 class GetOrCreatePropertyCardView(APIView):
   def post(self, request, *args, **kwargs):
-    if 'file' not in request.FILES:
+    if 'pcFile' not in request.FILES:
       return JsonResponse({'error': 'Expecting a file'}, status=400)
 
-    file = request.FILES['file']
-    property_card_data = self.read_property_card(file)
-    print(property_card_data,"*************************************")
+    file_uploaded = request.FILES['pcFile']
+    file_content = file_uploaded.read()
+    property_card_data = self.read_property_card(file_content)
+    
     
     
     
@@ -30,21 +31,29 @@ class GetOrCreatePropertyCardView(APIView):
       'id': "archivo_obj.id",
     })
     
-  def read_property_card(self, file: IO)-> dict:
+  def read_property_card(self, file_content: IO)-> dict:
     try:
-      url = settings.AZURE_MODEL_ENDPOINT,
-      file_base64 = base64.b64encode(file.read())
-      payload = json.dumps({"base64Source": file_base64})
+      url = settings.AZURE_MODEL_ANALYZER_ENDPOINT
+      file_base64 = base64.b64encode(file_content).decode('utf-8')
+      payload = json.dumps({
+        "base64Source": file_base64
+      })
+
       headers = {
         'Ocp-Apim-Subscription-Key': settings.AZURE_SECRET_KEY,
         'Content-Type': 'application/json'
       }
       response = requests.request("POST", url, headers=headers, data=payload)
       response.raise_for_status()
+      response_headers = response.headers
       data = response.json()
-      return data
+      print(response_headers,"************response_headers**************")
+      print(data,"************data**************")
+      
+      return "data"
     except Exception as e:
-      return {"error": str(e)}
+      print(e,"aeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeerror")
+      return e
 
 
 
